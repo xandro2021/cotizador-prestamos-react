9cfa8e4be036bf2f53d9649059d95fd738dfeacc
xandro2021 <xandrocastro2021@gmail.com>
2023-07-04 19:34:40 -0600

    Introduccion a los states en react (curso js juan)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
 function App() {
 
+  const [cantidad, setCantidad] = useState(10000);
+
+  function handleChange(e) {
+    console.log(Number(e.target.value))
+  }
+
   return (
-    <h1 className="text-6xl font-extrabold">Hola Mundo</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+
+      <Header/>
+
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+      />
+
+    </div>
   )
 }
 
